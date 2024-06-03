91253bd0659123f6f15f0d4e1beff4056e942803
Valeria <nelera.gb@gmail.com>
2024-06-03 04:38:49 +0300

    homework_03.06.2024

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ import multiprocessing
 import time
 import argparse
 import aiohttp
-import asyncio
 import requests
+import asyncio
 
 urls = [
     'https://t3.ftcdn.net/jpg/05/57/92/94/360_F_557929456_H0L12sooeJMSSnI98jMyqfzxd93dKOcP.jpg',
@@ -61,49 +61,39 @@ def save_image_multiprocessing(new_urls: list[str] = urls):
         f'{(time.time() - start_time):.2f} секунд')
 
 
-async def save_image_async(new_url):
+async def save_image_async(new_urls: list[str] = urls):
     extensions = ['png', 'jpg', 'jpeg', 'gif', 'tiff']
     path = os.path.join(os.getcwd(), 'images', 'async')
     if not os.path.exists(path):
         os.makedirs(path, exist_ok=True)
     start_time = time.time()
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for url in new_urls:
+            tasks.append(download_image_async(session, url, path, extensions,
+                                              start_time))
+        await asyncio.gather(*tasks)
+    print(
+        f'Общее время выполнения (асинхронно): {(time.time() - start_time):.2f} секунд')
+
+
+async def download_image_async(session, url, path, extensions, start_time):
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(new_url) as image:
-                file_name = new_url.split('/')[-1]
-                if file_name.split('.')[-1] in extensions:
-                    with open(os.path.join(path, file_name), 'wb') as file:
-                        file.write(await image.read())
-                    print(f'Время выполнения: '
-                          f'{(time.time() - start_time):.2f} секунд')
+        async with session.get(url) as image:
+            file_name = url.split('/')[-1]
+            if file_name.split('.')[-1] in extensions:
+                with open(os.path.join(path, file_name), 'wb') as file:
+                    file.write(await image.read())
+                print(f'Время выполнения: '
+                      f'{(time.time() - start_time):.2f} секунд')
     except Exception as e:
         print(e)
 
 
-async def save_images_async(new_urls: list[str] = urls):
-    tasks = []
-    start_time = time.time()
-    for url in new_urls:
-        task = asyncio.ensure_future(save_image_async(url))
-        tasks.append(task)
-    await asyncio.gather(*tasks)
-    print(
-        f'Общее время выполнения (асинхронность): '
-        f'{(time.time() - start_time):.2f} секунд')
-
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Save images from URLs')
-    parser.add_argument('urls', type=str, nargs='*',
-                        help='Введите адреса изображений')
-    args = parser.parse_args()
-    if args.urls:
-        save_image_threading(args.urls)
-        save_image_multiprocessing(args.urls)
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(save_images_async(args.urls))
-    else:
-        save_image_threading()
-        save_image_multiprocessing()
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(save_images_async())
+    # Выберите одну из функций для выполнения:
+    # save_image_threading()
+    # save_image_multiprocessing()
+
+    # Для асинхронного вызова:
+    asyncio.run(save_image_async(urls))
